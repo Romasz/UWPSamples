2aa6f8bdc83cd3156311e165fae0f4b6939403ef
Tomasz Romaszkiewicz <tomasz@romasz.net>
2017-01-31 08:03:45 +0100

    Added sample

---
+++ ItemTemplateSelectorSample/ItemTemplateSelectorSample/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ItemTemplateSelectorSample
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaiseProperty(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private bool isSwitched = false;
+        public bool IsSwitched
+        {
+            get { return isSwitched; }
+            set { isSwitched = value; RaiseProperty(nameof(IsSwitched)); }
+        }
+
+        public MainPage() { this.InitializeComponent(); }
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            ListOfItems.Items.Add(new ItemClass { Type = isSwitched ? ItemType.Greed : ItemType.Red, Text = "NEW ITEM" });
+        }
+    }
+
+    public enum ItemType { Red, Greed };
+
+    public class ItemClass
+    {
+        public ItemType Type { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class MySelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            switch ((item as ItemClass).Type)
+            {
+                case ItemType.Greed:
+                    return GreenTemplate;
+                case ItemType.Red:
+                default:
+                    return RedTemplate;
+            }
+        }
+
+        public DataTemplate GreenTemplate { get; set; }
+        public DataTemplate RedTemplate { get; set; }
+    }
+}
