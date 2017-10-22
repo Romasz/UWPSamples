1de9617d13848d1155f119ea48dc2ceb845bb6a7
Tomasz Romaszkiewicz <tomasz@romasz.net>
2017-10-22 17:17:57 +0200

    Sample trigger project.

---
+++ TextBlockTrigger/TextBlockTrigger/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TextBlockTrigger
+{
+    public class EqualParamConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language) => value.ToString() == parameter.ToString();
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+    }
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage() { this.InitializeComponent(); }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e) => myTextBlck.Text = (bool)(sender as ToggleButton).IsChecked ? "On" : "Off";
+    }
+}
