9c78f12b100c11ecc282fb967f523de30a5a9b8b
Tomasz Romaszkiewicz <tomasz@romasz.net>
2018-06-29 21:29:10 +0200

    Added sample

---
+++ ListItemTrigger/ListItemTrigger/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListItemTrigger
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<ShopItem> ShopList { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ShopList = new ObservableCollection<ShopItem>
+            {
+                new ShopItem{ Title = "First", IsBookAvailable = false},
+                new ShopItem{ Title = "Second", IsBookAvailable = false},
+                new ShopItem{ Title = "Third", IsBookAvailable = true},
+                new ShopItem{ Title = "Fourth", IsBookAvailable = false}
+            };
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var shopItem = (e.OriginalSource as FrameworkElement).DataContext as ShopItem;
+            shopItem.IsBookAvailable = !shopItem.IsBookAvailable;
+        }
+    }
+}
---
+++ ListItemTrigger/ListItemTrigger/ShopItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListItemTrigger
+{
+    public class ShopItem : INotifyPropertyChanged
+    {
+        public bool IsBookAvailable
+        {
+            get { return isBookAvailable; }
+            set {
+                if (isBookAvailable != value)
+                {
+                    isBookAvailable = value;
+                    Notify(nameof(IsBookAvailable));
+                }
+            }
+        }
+        private bool isBookAvailable = false;
+
+        public string Title { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Notify(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
